6a3aed695573e3dafaebe466fdc601e5ff52bd3e
Patrycjusz Ozlanski <patrycjusz.ozlanski@gmail.com>
2017-01-23 17:09:28 +0100

    bugfix for init migration

--- migrations/m160102_154310_init.php
+++ migrations/m160102_154310_init.php
@@ -21,21 +21,24 @@ class m160102_154310_init extends Migration
             // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
             $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
         }
-        $this->createTable($this->tableName, [
-            'id' => Schema::TYPE_PK,
-            'table_name' => Schema::TYPE_STRING . ' NOT NULL',
-            'row_id' => Schema::TYPE_INTEGER . ' NOT NULL',
-            'event' => Schema::TYPE_SMALLINT . ' NOT NULL',
-            'created_at' => Schema::TYPE_INTEGER . ' NOT NULL',
-            'created_by' => Schema::TYPE_INTEGER,
-            'field_name' => Schema::TYPE_STRING,
-            'old_value' => Schema::TYPE_TEXT,
-            'new_value' => Schema::TYPE_TEXT,
-        ]);
-        $this->createIndex('index-1', $this->tableName, 'table_name');
-        $this->createIndex('index-2', $this->tableName, ['table_name', 'row_id']);
-        $this->createIndex('index-3', $this->tableName, ['table_name', 'field_name']);
-        $this->createIndex('index-4', $this->tableName, 'event');
+		$tableSchema = Yii::$app->db->schema->getTableSchema($this->tableName);
+        if ($tableSchema === null) {
+		    $this->createTable($this->tableName, [
+		        'id' => Schema::TYPE_PK,
+		        'table_name' => Schema::TYPE_STRING . ' NOT NULL',
+		        'row_id' => Schema::TYPE_INTEGER . ' NOT NULL',
+		        'event' => Schema::TYPE_SMALLINT . ' NOT NULL',
+		        'created_at' => Schema::TYPE_INTEGER . ' NOT NULL',
+		        'created_by' => Schema::TYPE_INTEGER,
+		        'field_name' => Schema::TYPE_STRING,
+		        'old_value' => Schema::TYPE_TEXT,
+		        'new_value' => Schema::TYPE_TEXT,
+		    ]);
+		    $this->createIndex('index-1', $this->tableName, 'table_name');
+		    $this->createIndex('index-2', $this->tableName, ['table_name', 'row_id']);
+		    $this->createIndex('index-3', $this->tableName, ['table_name', 'field_name']);
+		    $this->createIndex('index-4', $this->tableName, 'event');
+		}
     }
 
     public function down()
